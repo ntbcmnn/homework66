520dceaf08165e4639c957ef8d937e02551ffdcc
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-02 23:29:01 +0600

    Created a constant for the type of meal

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const mealTime = [
+  {
+    id: 'breakfast', title: 'Breakfast',
+  },
+  {
+    id: 'lunch', title: 'Lunch',
+  },
+  {
+    id: 'snack', title: 'Snack',
+  },
+  {
+    id: 'dinner', title: 'Dinner',
+  }
+];
