ee5fefc3b22803e457bd9f27861c5109841c6e92
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-02 23:34:56 +0600

    Created Routes and wrapped them into Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import './App.css';
+import Layout from './Components/Layout/Layout.tsx';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Containers/Home/Home.tsx';
+import AddMeal from './Containers/AddMeal/AddMeal.tsx';
+import EditMeal from './Containers/EditMeal/EditMeal.tsx';
 
-const App = () => <></>;
+const App = () => <>
+  <Layout>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/home/add" element={<AddMeal/>}/>
+      <Route path="home/:mealId/edit" element={<EditMeal/>}/>
+      <Route path="*" element={<h2>Page not found</h2>}/>
+    </Routes>
+  </Layout>
+</>;
 
 export default App;
