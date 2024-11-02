a2bfe58a075d02de9e6cb056fda40fcdbdfdb69b
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-02 23:32:15 +0600

    Added 2 types of pre-loaders: 1 for the buttons & 1 for the app rendering/reloading

---
+++ src/Components/UI/ButtonSpinner/ButtonSpinner.tsx
@@ -0,0 +1,10 @@
+const ButtonSpinner = () => {
+  return (
+    <>
+      <span className="spinner-border spinner-border-sm"></span>
+      <span className="visually-hidden">Loading...</span>
+    </>
+  );
+};
+
+export default ButtonSpinner;
